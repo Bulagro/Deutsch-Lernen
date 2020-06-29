dded7c685e6dcf4248d11208067365954e4136cf
Bulagro <christumer@gmail.com>
2020-06-29 02:26:14 -0500

    Function to merge same-meaninged words into same entry.
    This merges those entries whose meanings in spanish are the same.
    It's not very efficient, but it gets the job done.

--- manage_words_dict.py
+++ manage_words_dict.py
@@ -16,4 +16,32 @@ def translate_old_dict():
             words_file.write(json.dumps(words, indent=4, sort_keys=True))
 
 def merge_same_meanigns_into_same_entry():
-    pass
+    words = json.load(open('words.json', 'r'))
+
+    while True:
+        total_keys = len(words.keys())
+        done = True
+        for i in range(total_keys):
+            if str(i) in words.keys():
+                es_meanings = words[str(i)]['es']
+
+                for j in range(i + 1, total_keys):
+                    if str(j) in words.keys():
+                        if words[str(j)]['es'] == es_meanings and i != j:
+                            words[str(i)]['de'] += words[str(j)]['de']
+                            words.pop(str(j))
+                            done = False
+                            print(f'Remove entry {j} (same meaning as {i})')
+                            break
+
+            if not done:
+                break
+
+        if done:
+            with open('words.json', 'w') as f:
+                f.write(json.dumps(words, indent=4, sort_keys=True))
+            return True
+
+
+# translate_old_dict()
+# merge_same_meanigns_into_same_entry()
