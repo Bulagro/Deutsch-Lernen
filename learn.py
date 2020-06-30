2cf1a2be44f8f0d39c3dfaee50d3939c4eca3631
Bulagro <christumer@gmail.com>
2020-06-29 23:31:59 -0500

    Start work on main 'learning file'.
    This file is the way the user interacts with the dictionary.
    It works by 'sessions'. Each word has it's own score (the 'points'
    entry); this script selects the first n words with the lowest score.
    
    Words get points when their meaning is correctly written.

---
+++ learn.py
@@ -0,0 +1,29 @@
+import json
+
+# Select n number of words (that's a session).
+# display them in a given direction (zB. german -> spanish)
+# update
+
+def get_words(WORDS_PER_SESSION=100):
+    """ Returns a list with WORDS_PER_SESSION words. """
+    with open('words.json', 'r') as words_file:
+        w = json.load(words_file)
+        words_list = []
+
+        min_points = 0
+        while len(words_list) < WORDS_PER_SESSION:
+            for word in w:
+                if word == 'total': continue
+
+                if w[word]['points'] <= min_points:
+                    words_list.append(w[word])
+
+                    if len(words_list) == WORDS_PER_SESSION:
+                        break
+            else:
+                min_points += 1
+
+        return words_list
+
+def main():
+    pass
