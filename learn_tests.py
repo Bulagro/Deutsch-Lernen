6a73f412b0159fddcaf69de27cd6e63253141fb6
Bulagro <christumer@gmail.com>
2020-07-05 01:46:05 -0500

    Tests for compare_words().

--- learn_tests.py
+++ learn_tests.py
@@ -15,5 +15,35 @@ class TestGetWords(unittest.TestCase):
         self.assertEqual(100, len(w))
         self.assertEqual(expected_words_list, w)
 
+
+class TestCompareWords(unittest.TestCase):
+    def test_compare_almost_equal_words(self):
+        actual = learn.compare_words('hola', 'ohla')
+        expected = 0
+
+        self.assertEqual(expected, actual)
+
+        actual = learn.compare_words('Pudín', 'Puídn')
+        expected = 2
+
+        self.assertEqual(expected, actual)
+
+    def test_compare_different_words(self):
+        actual = learn.compare_words('adiós', 'hola')
+
+        self.assertFalse(actual)
+
+    def test_compare_equal_words(self):
+        actual = learn.compare_words('manzana', 'manzana')
+
+        self.assertTrue(actual)
+
+    def test_empty_strings(self):
+        actual = learn.compare_words('', '')
+
+        self.assertTrue(actual)
+
+
+
 if __name__ == "__main__":
     unittest.main()
