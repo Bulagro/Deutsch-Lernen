dd01c32de11a325aebab89daf85d14de29893051
Bulagro <christumer@gmail.com>
2020-07-05 00:41:38 -0500

    Modify select_words() to use get_words().

--- learn.py
+++ learn.py
@@ -15,24 +15,23 @@ def get_words(WORDS_PER_SESSION=100):
 def select_words(WORDS_PER_SESSION=100):
     """ Returns a list with WORDS_PER_SESSION words. """
 
-    with open('words.json', 'r') as words_file:
-        w = json.load(words_file)
-        words_list = []
+    t, w = get_words()
 
-        min_points = 0
-        while len(words_list) < WORDS_PER_SESSION:
-            for word in w:
-                if word == 'total': break
+    words_list = []
+    min_points = 0
+    while len(words_list) < WORDS_PER_SESSION:
+        for word in w:
+            if word == 'total': break
 
-                if w[word]['points'] <= min_points:
-                    words_list.append(w[word])
+            if w[word]['points'] <= min_points:
+                words_list.append(w[word])
 
-                    if len(words_list) == WORDS_PER_SESSION:
-                        break
+                if len(words_list) == WORDS_PER_SESSION:
+                    break
             else:
                 min_points += 1
 
-        return words_list
+    return words_list
 
 def main():
     pass
