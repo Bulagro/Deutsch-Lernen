e1f4c8cfc58c05295fb3219c37363cef764aba1c
Bulagro <christumer@gmail.com>
2020-08-10 03:16:20 -0500

    add_word_to_dict().

--- manage_words_dict.py
+++ manage_words_dict.py
@@ -8,4 +8,18 @@ def change_point_counter(P: int):
     with open('words.json', 'w') as f:
         f.write(json.dumps(words, indent=4, sort_keys=True))
 
-# change_point_counter(1)
+
+def add_word_to_dict(de: list, es: list, words_file='words.json'):
+    words = json.load(open(words_file, 'r'))
+
+    word_id = str(len(words))
+    word_dict_entry = {
+        "de": de,
+        "es": es,
+        "points" : 0,
+    }
+
+    words[word_id] = word_dict_entry
+
+    with open(words_file, 'w') as w:
+        w.write(json.dumps(words, indent=4))
