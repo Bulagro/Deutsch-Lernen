26508bd514e7cfc29d9f7c2b5f8561d2fb521ee2
Bulagro <christumer@gmail.com>
2020-08-10 03:40:07 -0500

    words_file -> WORDS_FILE.

--- manage_words_dict.py
+++ manage_words_dict.py
@@ -6,11 +6,11 @@ def change_point_counter(P: int):
         words[w]['points'] = P
 
     with open('words.json', 'w') as f:
-        f.write(json.dumps(words, indent=4, sort_keys=True))
+        f.write(json.dumps(words, indent=4))
 
 
-def add_word_to_dict(de: list, es: list, words_file='words.json'):
-    words = json.load(open(words_file, 'r'))
+def add_word_to_dict(de: list, es: list, WORDS_FILE='words.json'):
+    words = json.load(open(WORDS_FILE, 'r'))
 
     word_id = str(len(words))
     word_dict_entry = {
@@ -21,5 +21,5 @@ def add_word_to_dict(de: list, es: list, words_file='words.json'):
 
     words[word_id] = word_dict_entry
 
-    with open(words_file, 'w') as w:
+    with open(WORDS_FILE, 'w') as w:
         w.write(json.dumps(words, indent=4))
