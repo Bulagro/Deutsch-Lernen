30f29485a7d7b51cd7e77f62fad83f7355f5e769
Bulagro <christumer@gmail.com>
2020-08-10 03:43:25 -0500

    Adapt test to new attributes for learn.select_words

--- learn_tests.py
+++ learn_tests.py
@@ -6,13 +6,12 @@ total_words, complete_words_list = learn.get_words()
 
 class TestSelectWords(unittest.TestCase):
     def test_get_first_100_words(self):
-        w = learn.select_words(100)
+        w = learn.select_words(WORDS_PER_SESSION=100)
 
         expected_words_list = [
             complete_words_list[str(i)] for i in range(100)
         ]
 
-        self.assertEqual(100, len(w))
         self.assertEqual(expected_words_list, w)
 
 
