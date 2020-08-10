4c22f2c00f8b7da3c7d41405b9d77f815b68621a
Bulagro <christumer@gmail.com>
2020-08-10 02:54:41 -0500

    Remove 'total' from words.json

--- learn.py
+++ learn.py
@@ -1,25 +1,21 @@
 import json
 
 
-def get_words(WORDS_PER_SESSION=100):
+def get_words():
     with open('words.json', 'r') as f:
         words = json.load(f)
-        total = words['total']
-        del words['total']
 
-        return(total, words)
+        return(len(words), words)
 
 def select_words(WORDS_PER_SESSION=100):
     """ Returns a list with WORDS_PER_SESSION words. """
 
-    t, w = get_words()
+    _, w = get_words()
 
     words_list = []
     min_points = 0
     while len(words_list) < WORDS_PER_SESSION:
         for word in w:
-            if word == 'total': break
-
             if w[word]['points'] <= min_points:
                 words_list.append(w[word])
 
--- manage_words_dict.py
+++ manage_words_dict.py
@@ -3,7 +3,6 @@ import json
 def change_point_counter(P: int):
     words = json.load(open('words.json', 'r'))
     for w in words:
-        if w == 'total': break;
         words[w]['points'] = P
 
     with open('words.json', 'w') as f:
